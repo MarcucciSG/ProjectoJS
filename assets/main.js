890935d8468f15025c0822aafdb4d4f77b8ff882
SantiagoMarcucci <marcuccisantiago8@gmail.com>
2023-03-16 19:12:06 -0300

    arrancando proyecto JS

---
+++ assets/main.js
@@ -0,0 +1,45 @@
+//console.log(superHeroes)
+
+
+const datosBusqueda = {
+    empresa:'' + prompt('ingresar empresa: marvel o dc comics'),
+    año: + prompt('Elegir año entre 1930 a 1960'),
+    residencia:'',
+    poderOhabilidad:'',
+}
+
+function mostrarHeroes(superHeroes){
+    superHeroes.forEach(heroe => {
+        console.log(heroe)        
+    });
+}
+
+function filtrarEmpresa(heroe){
+    if(datosBusqueda.empresa){
+        return heroe.empresa === datosBusqueda.empresa
+    }
+    return heroe
+}
+function filtrarAño(heroe){
+    if(datosBusqueda.año){
+        return heroe.año === datosBusqueda.año
+    }
+    return heroe
+}
+
+
+
+function noResultado(){
+    console.log('No hay resultados')
+}
+
+function filtraHeroe(){
+    let resultado = superHeroes.filter(filtrarEmpresa).filter(filtrarAño)
+        if(resultado.length){
+        mostrarHeroes(resultado)
+    }else{
+        noResultado()
+    }
+}
+
+filtraHeroe()
